6ce733e9de8b29081e7077c0b03d82349023c9ad
bnm03090805 <bnm03090805@gmail.com>
2025-03-10 11:23:35 +0900

    [feat] 레이저 트랩 걸렷을때 적 몬스터 생성

--- Assets/Script/Laser/LaserTrap.cs
+++ Assets/Script/Laser/LaserTrap.cs
@@ -7,19 +7,30 @@ public class LaserTrap : MonoBehaviour
     RaycastHit hit;
     public GameObject Capsule;
     public LayerMask playerLayer;
+    public GameObject spawnMonster;
+    private bool isSpwan = false;
+    public Transform enemySpawnPoint;
 
     // Update is called once per frame
     void Update()
     {
         Debug.DrawRay(transform.position, CapslueDirection());
-        if (Physics.Raycast(transform.position, CapslueDirection(), out hit, 20f , playerLayer))
+        if (Physics.Raycast(transform.position, CapslueDirection(), out hit, 20f , playerLayer) && !isSpwan)
         {
-            Debug.Log(hit.transform.name);
+            StartCoroutine(SpwanMonster());
         }
     }
 
     Vector3 CapslueDirection()
     {
         return Capsule.transform.position - transform.position;
+    }
+
+    IEnumerator SpwanMonster()
+    {
+        isSpwan = true;
+        Instantiate(spawnMonster, enemySpawnPoint);
+        yield return new WaitForSeconds(10f);
+        isSpwan = false;
     }
 }
